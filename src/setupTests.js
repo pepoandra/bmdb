303626cb6fd7e789a7692edc9053ffabb6dd2675
pepoandra <sebastian.andrade@ssense.com>
2021-03-13 17:04:52 -0500

    winging it

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
-// expect(element).toHaveTextContent(/react/i) wtf 2.0
+// expect(element).toHaveTextContent(/react/i) wtf 2.0 q csho
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
